699e4b4385ad9133e5ccbfdcf71b8c74695b042c
Adam Lindgren <adam@adamlindgren.com>
2022-02-22 19:53:51 -0500

    can correctly compile the list of words without punctuation or stop_words and the amount of times those words are used

--- word_frequency.py
+++ word_frequency.py
@@ -1,13 +1,26 @@
 STOP_WORDS = [
-    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
-    'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
-    'will', 'with'
+    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from',
+    'has', 'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on',
+    'that', 'the', 'to', 'were', 'will', 'with'
 ]
 
-
 def print_word_freq(file):
+    import string
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    finalWords = {}
+    with open(file) as file:
+        original = file.read()
+        words = original.translate(str.maketrans('', '', string.punctuation))
+        words = words.lower().split()
+        punc = '''!()-[]{};:'",<>./?@#$%^&*_~'''
+        for word in words:
+            if word in STOP_WORDS:
+                word = ''
+            if word in finalWords:
+                finalWords[f"{word}"] += 1
+            if word not in finalWords:
+                finalWords[f"{word}"] = 1
+    print(finalWords)
 
 
 if __name__ == "__main__":
